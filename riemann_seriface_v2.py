42b93cb09d6e14d0115d68979ac083132919e48e
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2021-07-19 21:27:27 +0200

    riemann_surface_v2 improved

--- riemann_seriface_v2.py
+++ riemann_seriface_v2.py
@@ -4,13 +4,10 @@ from plotly.offline import init_notebook_mode
 
 init_notebook_mode(connected=True)
 
-xs = np.linspace(-5, 5, 1000)
-ys = np.linspace(-5, 5, 1000)
+Phi, R = np.mgrid[0:4*np.pi:100j, -5.:5.:100j]
+X, Y = (R * np.cos(Phi), R * np.sin(Phi))
 
-X, Y = np.meshgrid(xs, ys)
-Z = X + 1j*Y
-
-fig = go.Figure(data=[go.Surface(x=X, y=Y, z=np.angle(Z))])
+fig = go.Figure(data=[go.Surface(x=X, y=Y, z=Phi)])
 fig.update_layout(title='Riemann Surface', autosize=False,
                   width=1000, height=1000,
                   margin=dict(l=65, r=50, b=65, t=90))
