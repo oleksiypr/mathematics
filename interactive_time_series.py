542f0aee15110a36e4ceb967d252b7aca5c7c062
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2021-07-19 21:32:19 +0200

    interactive_time_series second y-axis added

--- interactive_time_series.py
+++ interactive_time_series.py
@@ -5,7 +5,6 @@ from plotly.offline import init_notebook_mode
 init_notebook_mode(connected=True)
 
 # Read in data with two headers
-
 df = pd.read_csv('building_one.csv', header=[0,1], index_col=0)
 
 # Extract series from multi-index
@@ -20,9 +19,15 @@ steam_data = go.Scatter(x=steam_series.index,
                         y=steam_series.values,
                         yaxis='y2')
 
-layout = go.Layout(title='Energy Plot',
+layout = go.Layout(height=600, width=800,
+                   title='Energy and Steam Plot',
                    xaxis=dict(title='Date'),
-                   yaxis=dict(title='(kWh)'))
+                   yaxis=dict(title='Energy', color='red'),
+                   yaxis2=dict(title='Steam',
+                               color='blue',
+                               overlaying='y',
+                               side='right')
+                   )
 
-fig = go.Figure(data=[energy_data], layout=layout)
+fig = go.Figure(data=[energy_data, steam_data], layout=layout)
 fig.write_html('energy_plot.html', auto_open=True)
