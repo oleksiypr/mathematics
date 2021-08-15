cb72a6dfbfc38b89cd66b293be7c8f18c28a8357
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2021-08-15 10:28:31 +0200

    shadow solved

--- shadow.py
+++ shadow.py
@@ -1,75 +1,46 @@
 import plotly.graph_objs as go
 import numpy as np
-import numpy.linalg as la
 
 
 t = np.linspace(0, 20, 100)
 x, y, z = np.cos(t), np.sin(t), t
 
 R = np.array((x, y, z))
+s = np.array([1., 1., -2])
 
 A = np.array([
-    [1., 0., 0.],
-    [0., 1., 1.],
-    [0., 0., 0.]
+    [1., 0., -s[0]/s[2]],
+    [0., 1., -s[1]/s[2]],
+    [0., 0.,         0.]
 ])
 
 R1 = A @ R
 
 fig = go.Figure()
 
-'''
-fig_orig = go.Figure(data=go.Scatter3d(x=x, y=y, z=z))
-
-fig_shadow = go.Figure(
-    data=go.Scatter3d(
-        x=R1[0, :],
-        y=R1[1, :],
-        z=R1[2, :]
-    )
-)
-'''
-# tight layout
-
 fig.add_trace(
     go.Scatter3d(
         x=R1[0, :],
         y=R1[1, :],
-        z=R1[2, :]
+        z=R1[2, :],
+        mode="lines"
     ))
 
 
-fig.add_trace(go.Scatter3d(x=x, y=y, z=z))
+fig.add_trace(go.Scatter3d(
+    x=x, y=y, z=z,
+    mode="lines"
+))
 
-# fig = fig_shadow
 fig.update_layout(
     autosize=False,
-    xaxis=go.layout.XAxis(
-      range=[-150, 150],
-      showgrid=True,
-      zeroline=True,
-      showline=True,
-      gridcolor='#bdbdbd',
-      gridwidth=2,
-      zerolinecolor='#969696',
-      zerolinewidth=4,
-      linecolor='#636363',
-      linewidth=6
-    ),
-    yaxis=go.layout.YAxis(
-      range=[-150, 150],
-      showgrid=True,
-      zeroline=True,
-      showline=True,
-      gridcolor='#bdbdbd',
-      gridwidth=2,
-      zerolinecolor='#969696',
-      zerolinewidth=4,
-      linecolor='#636363',
-      linewidth=6
-    ),
-    height=600,
-    width=600,
-)
+    scene=dict(
+        xaxis=dict(nticks=4, range=[-10, 10],),
+        yaxis=dict(nticks=4, range=[-10, 10],),
+        zaxis=dict(nticks=4, range=[  0, 20],),),
+    width=800,
+    height=800,
+    margin=dict(r=20, l=10, b=10, t=10))
+
 
 fig.show()
