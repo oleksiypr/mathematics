983be44aae71484b4cc28ea002263ea264a72243
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2021-08-16 13:30:43 +0200

    reflection solved: with tensor contraction

--- reflection.py
+++ reflection.py
@@ -2,67 +2,49 @@ import plotly.graph_objs as go
 import numpy as np
 from numpy.linalg import inv
 
+# original data
+t = np.linspace(0, 20, 100)
+x, y, z = np.cos(t), np.sin(t) - 2.0, t
+
+# reflection parameter
 theta_degrees = 60.
 theta = np.radians(theta_degrees)
 
+# mirror plane matrix
 E = np.array([
     [1.,            0.,             0.],
     [0., np.cos(theta), -np.sin(theta)],
     [0., np.sin(theta),  np.cos(theta)]
 ])
 
+# reflection matrix
 TE = np.array([
-    [1., 0., 0.],
-    [0., 1., 0.],
+    [1., 0.,  0.],
+    [0., 1.,  0.],
     [0., 0., -1.]
 ])
 
-T = E @ TE @ inv(E)
-
-t = np.linspace(0, 20, 100)
-x, y, z = np.cos(t), np.sin(t) - 2.0, t
-
-R = np.array((x, y, z))
+# reflection
+T  = E @ TE @ inv(E)
+R  = np.array([x, y, z])
 R1 = T @ R
 
-# mirror plane
-'''
-x1 = np.linspace(-10., 10., 5)
-y1 = np.linspace(-10., 10., 5)
-z1 = np.zeros(5)
-M1 = np.array((x1, y1, z1))
-M = E @ M1
-'''
+# mirror plane coordinates
+X1, Y1 = np.meshgrid(np.linspace(-10., 10., 5), np.linspace(- 0., 10., 5))
+Z1 = np.zeros([5, 5])
 
+# mirror plane coordinate tensor of rank 3
+M1 = np.array([X1, Y1, Z1])
+
+# mirror plane
+M = np.tensordot(E, M1, 1)
+
+# 3D picture
 fig = go.Figure()
 
-fig.add_trace(
-    go.Scatter3d(
-        x=R1[0, :],
-        y=R1[1, :],
-        z=R1[2, :],
-        mode="lines"
-    ))
-
-
-fig.add_trace(go.Scatter3d(
-    x=x, y=y, z=z,
-    mode="lines"
-))
-
-
-X, Y = np.meshgrid(
-    np.linspace(-10., 10., 5),
-    np.linspace(- 0., 10., 5)
-)
-
-fig.add_trace(go.Surface(
-    x=X,
-    y=Y,
-    z=Y * np.tan(theta),
-    opacity=0.5
-))
-
+fig.add_trace(go.Scatter3d(x=R1[0, :], y=R1[1, :], z=R1[2, :], mode="lines"))
+fig.add_trace(go.Scatter3d(x=x, y=y, z=z, mode="lines"))
+fig.add_trace(go.Surface(x=M[0], y=M[1], z=M[2],opacity=0.5))
 
 fig.update_layout(
     autosize=False,
